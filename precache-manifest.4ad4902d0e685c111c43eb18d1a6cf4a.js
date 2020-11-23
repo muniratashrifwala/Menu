e1143997a2d58dda2313f3122265f5fa197e3936
Munira Tashrifwala <munira.i.tashrifwala@gmail.com>
2020-11-23 09:59:00 +0530

    Updates

---
+++ precache-manifest.4ad4902d0e685c111c43eb18d1a6cf4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64d280ea6854ec474970960455da523",
+    "url": "/MenuList/index.html"
+  },
+  {
+    "revision": "76e57858f5f6e90d9cf0",
+    "url": "/MenuList/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/MenuList/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MenuList/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e57858f5f6e90d9cf0",
+    "url": "/MenuList/static/js/main.6ea6756f.chunk.js"
+  },
+  {
+    "revision": "464cdd12ac2f9eeff581",
+    "url": "/MenuList/static/js/runtime-main.70ae82ad.js"
+  }
+]);
